55c4fe003725b7227bf1c4268abf13c6fa3eea96
gramsa23 <89297908+gramsa23@users.noreply.github.com>
2022-03-16 13:20:31 +0300

    markdown

--- library.py
+++ library.py
@@ -1,56 +1,56 @@
 import textwrap
 
 INITIAL_PERMUTATION_TABLE = [
-                            '58 ', '50 ', '42 ', '34 ', '26 ', '18 ', '10 ', '2',
-			                '60 ', '52 ', '44 ', '36 ', '28 ', '20 ', '12 ', '4',
-			                '62 ', '54 ', '46 ', '38 ', '30 ', '22 ', '14 ', '6', 
-			                '64 ', '56 ', '48 ', '40 ', '32 ', '24 ', '16 ', '8', 
-			                '57 ', '49 ', '41 ', '33 ', '25 ', '17 ', '9 ', '1',
-			                '59 ', '51 ', '43 ', '35 ', '27 ', '19 ', '11 ', '3',
-			                '61 ', '53 ', '45 ', '37 ', '29 ', '21 ', '13 ', '5',
-			                '63 ', '55 ', '47 ', '39 ', '31 ', '23 ', '15 ', '7'
+58,50,42,34,26,18,10,2,
+60,52,44,36, 28 , 20 , 12 , 4,
+62 , 54 , 46 , 38 , 30 , 22 , 14 , 6, 
+64 , 56 , 48 , 40 , 32 , 24 , 16 , 8, 
+57 , 49 , 41 , 33 , 25 , 17 , 9 , 1,
+59 , 51 , 43 , 35 , 27 , 19 , 11 , 3,
+61 , 53 , 45 , 37 , 29 , 21 , 13 , 5,
+63 , 55 , 47 , 39 , 31 , 23 , 15 , 7
 ]
 
 INVERSE_PERMUTATION_TABLE = [
-                            '40 ', '8 ', '48 ', '16 ', '56 ', '24 ', '64 ', '32',
-			                '39 ', '7 ', '47 ', '15 ', '55 ', '23 ', '63 ', '31',
-			                '38 ', '6 ', '46 ', '14 ',  '54 ', '22 ', '62 ', '30',
-			                '37 ', '5 ', '45 ', '13 ', '53 ', '21 ', '61 ', '29',
-			                '36 ', '4 ', '44 ', '12 ', '52 ', '20 ', '60 ', '28',
-			                '35 ', '3 ', '43 ', '11 ', '51 ', '19 ', '59 ', '27', 
-			                '34 ', '2 ', '42 ', '10 ', '50 ', '18 ', '58 ', '26',
-			                '33 ', '1 ', '41 ', '9 ', '49 ', '17 ', '57 ', '25'
+40 , 8 , 48 , 16 , 56 , 24 , 64 , 32,
+39 , 7 , 47 , 15 , 55 , 23 , 63 , 31,
+38 , 6 , 46 , 14 ,  54 , 22 , 62 , 30,
+37 , 5 , 45 , 13 , 53 , 21 , 61 , 29,
+36 , 4 , 44 , 12 , 52 , 20 , 60 , 28,
+35 , 3 , 43 , 11 , 51 , 19 , 59 , 27, 
+34 , 2 , 42 , 10 , 50 , 18 , 58 , 26,
+33 , 1 , 41 , 9 , 49 , 17 , 57 , 25
 ]
 
 PC1_TABLE = [
-    57,49,41,33,25,17,9,
-    1,58,50,42,34,26,18,
-    10,2,59,51,43,35,27,
-    19,11,3,60,52,44,36,
-    63,55,47,39,31,23,15,
-    7,62,54,46,38,30,22,
-    14,6,61,53,45,37,29,
-    21,13,5,28,20,12,4
+57,49,41,33,25,17,9,
+1,58,50,42,34,26,18,
+10,2,59,51,43,35,27,
+19,11,3,60,52,44,36,
+63,55,47,39,31,23,15,
+7,62,54,46,38,30,22,
+14,6,61,53,45,37,29,
+21,13,5,28,20,12,4
 ]
 
 PC2_TABLE = [
-    14,17,11,24,1,5,3,28,
-    15,6,21,10,23,19,12,4,
-    26,8,16,7,27,20,13,2,
-    41,52,31,37,47,55,30,40,
-    51,45,33,48,44,49,39,56,
-    34,53,46,42,50,36,29,32
+14,17,11,24,1,5,3,28,
+15,6,21,10,23,19,12,4,
+26,8,16,7,27,20,13,2,
+41,52,31,37,47,55,30,40,
+51,45,33,48,44,49,39,56,
+34,53,46,42,50,36,29,32
 ]
 
 EXPANSION_TABLE = [
-    32,1,2,3,4,5,
-    4,5,6,7,8,9,
-    8,9,10,11,12,13,
-    12,13,14,15,16,17,
-    16,17,18,19,20,21,
-    20,21,22,23,24,25,
-    24,25,26,27,28,29,
-    28,29,30,31,32,1
+32,1,2,3,4,5,
+4,5,6,7,8,9,
+8,9,10,11,12,13,
+12,13,14,15,16,17,
+16,17,18,19,20,21,
+20,21,22,23,24,25,
+24,25,26,27,28,29,
+28,29,30,31,32,1
 ]
 
 SBOX = [
@@ -61,10 +61,10 @@ SBOX = [
 ]
 
 PERMUTATION_TABLE = [
-    16,7,20,21,29,12,28,17,
-    1,15,23,26,5,18,31,10,
-	2,8,24,14,32,27,3,9,
-    19,13,30,6,22,11,4,25
+16,7,20,21,29,12,28,17,
+1,15,23,26,5,18,31,10,
+2,8,24,14,32,27,3,9,
+19,13,30,6,22,11,4,25
 ]
 
 # 1. Adım açık metine karşılık gelen ikili kod
@@ -73,9 +73,9 @@ def string_to_binary(plaintext):
     return binary_plaintext
 
 # 2. Adım IP uygulanması
-def apply_initial_permutation(binary_plaintext, PERMUTATION_TABLE=INITIAL_PERMUTATION_TABLE):
+def apply_initial_permutation(binary_plaintext, TABLE=INITIAL_PERMUTATION_TABLE):
     permutated_text = ""
-    for index in PERMUTATION_TABLE:
+    for index in TABLE:
         permutated_text += binary_plaintext[int(index)-1]
     return permutated_text
 
@@ -85,7 +85,7 @@ def split_in_half(binarybits):
     R0 = binarybits[32:]
     return L0,R0
 
-# 4.Adım Expansion Table
+# 4.Adım Expansion Table 
 def apply_expansion(R0,PERMUTATION_TABLE=EXPANSION_TABLE):
     bits48 = ""
     for index in EXPANSION_TABLE:
@@ -142,7 +142,7 @@ def apply_permutation(sbox_32bits,PERMUTATION_TABLE=PERMUTATION_TABLE):
 # F FONKSYONU
 def functionF(pre32bits, key48bits):	
 	result = ""
-	expanded_left_half = apply_expansion(EXPANSION_TABLE,pre32bits)
+	expanded_left_half = apply_expansion(pre32bits)
 	xor_value = XOR(expanded_left_half,key48bits)
 	bits6list = split_in_6bits(xor_value)
 	for sboxcount, bits6 in enumerate(bits6list):
@@ -150,7 +150,7 @@ def functionF(pre32bits, key48bits):
 		middle4 = get_middle_four_bit(bits6)
 		sboxvalue = sbox_lookup(sboxcount,first_last,middle4)
 		result += sboxvalue
-	final32bits = apply_permutation(PERMUTATION_TABLE,result)	
+	final32bits = apply_permutation(result)	
 	return final32bits
 
 ###########################################################
